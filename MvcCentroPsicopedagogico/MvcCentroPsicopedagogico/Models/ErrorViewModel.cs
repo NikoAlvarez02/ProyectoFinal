d5a5d7af14eea12ac6e029a14fd5ab3ca43be6ec
belu <mbesanchez0@gmail.com>
2025-06-21 20:45:08 -0300

    se agrega login

--- MvcCentroPsicopedagogico/MvcCentroPsicopedagogico/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MvcCentroPsicopedagogico.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
